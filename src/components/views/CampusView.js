146a3a155c8fdead7554fae601dba8f578cbde49
melkadze <25963683+melkadze@users.noreply.github.com>
2023-05-12 00:45:59 -0400

    display more information on individual pages

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -9,25 +9,41 @@ import { Link } from "react-router-dom";
 // Take in props data to construct the component
 const CampusView = (props) => {
   const {campus} = props;
-  
-  // Render a single Campus view with list of its students
-  return (
-    <div>
-      <h1>{campus.name}</h1>
-      <p>{campus.address}</p>
-      <p>{campus.description}</p>
-      {campus.students.map( student => {
-        let name = student.firstname + " " + student.lastname;
+    
+    
+    if (!campus.students.length) {
+        // Render a single Campus view with list of its students
         return (
-          <div key={student.id}>
-            <Link to={`/student/${student.id}`}>
-              <h2>{name}</h2>
-            </Link>             
-          </div>
-        );
-      })}
-    </div>
-  );
+                <div>
+                <h1>{campus.name}</h1>
+                <img src={campus.imageUrl} alt="Campus"></img>
+                <p>{campus.address}</p>
+                <p>{campus.description}</p>
+                <h2>There are no enrolled students.</h2>
+                </div>
+                );
+    } else {
+        
+        // Render a single Campus view with list of its students
+        return (
+                <div>
+                <h1>{campus.name}</h1>
+                <img src={campus.imageUrl} alt="Campus"></img>
+                <p>{campus.address}</p>
+                <p>{campus.description}</p>
+                {campus.students.map( student => {
+                    let name = student.firstname + " " + student.lastname;
+                    return (
+                            <div key={student.id}>
+                            <Link to={`/student/${student.id}`}>
+                            <h2>{name}</h2>
+                            </Link>
+                            </div>
+                            );
+                })}
+                </div>
+                );
+    }
 };
 
 export default CampusView;
--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -6,14 +6,31 @@ It constructs a React component to display the single student view page.
 ================================================== */
 const StudentView = (props) => {
   const { student } = props;
-
-  // Render a single Student view 
-  return (
-    <div>
-      <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
-    </div>
-  );
+    
+    
+    if (!student.campus) {
+        return (
+                <div>
+                <h1>{student.firstname + " " + student.lastname}</h1>
+                <img src={student.imageUrl} alt="Student"></img>
+                <p>{student.gpa}</p>
+                <p>{student.email}</p>
+                <h3>This student is currently not enrolled anywhere.</h3>
+                </div>
+                );
+    } else {
+        
+        // Render a single Student view
+        return (
+                <div>
+                <h1>{student.firstname + " " + student.lastname}</h1>
+                <img src={student.imageUrl} alt="Student"></img>
+                <p>{student.gpa}</p>
+                <p>{student.email}</p>
+                <h3>{student.campus.name}</h3>
+                </div>
+                );
+    }
 
 };
 
